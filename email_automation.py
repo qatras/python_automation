73fa3f1997879a57dde80675bc4e432cabf9f348
qatras <78110681+qatras@users.noreply.github.com>
2021-11-19 22:01:12 +0400

    Update email_automation.py

--- email_automation.py
+++ email_automation.py
@@ -1,15 +1,14 @@
 
 
 
-with open("file.txt",'r') as file:     #type your txt filename instead of file1.txt
+with open("file.txt",'r') as file:     #type yourown txt file instead of file.txt
     lines = [line.strip() for line in file]
 
 with open("gm.txt",'w') as file: #new txt file will be 'gm.txt' for gmail
     for line in lines:
         if (line.split("@")[1])=="gmail.com":
             file.write(line + " \n")
-            #file.write(line + ",-by su-\n") for add more words
-
+           
 with open("ym.txt",'w') as file: #new txt file will be "ym.txt" for yahoo
     for line in lines:
         if (line.split("@")[1])=="yahoo.com":
